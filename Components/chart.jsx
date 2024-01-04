3645c8b0e2dd9e567f3e64cf78b72633a24c787e
pabbabhanuprasad <pabbabhanuprasad1@gmail.com>
2024-01-04 15:08:36 +0530

    addig fi;e

---
+++ Components/chart.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import Square from "./square";
+const Chart = () => {
+  const [state, setState] = useState(Array(9).fill(null));
+  // console.log(state);
+  const [isXTurn, setXTurn] = useState(true);
+
+  const checkWinner=()=>{
+    const winnerLogic=[
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    for(let logic of winnerLogic){
+        const[a,b,c]=logic;
+        if(state[a]!==null&&state[a]===state[b]&&state[a]===state[c]){
+          console.log(a);
+            return state[a];
+
+        }
+    }
+    return false;
+  };
+  const winnerIs = checkWinner();
+
+  const handleClick = (index) => {
+    if(state[index]!==null){
+        return;
+    }
+    const copyState = [...state];
+    copyState[index] = isXTurn ? "X" : "O";
+    setState(copyState);
+    setXTurn(!isXTurn);
+  };
+
+const handleReset=()=>{
+    setState(Array(9).fill(null));
+}
+
+  return (
+    <div className="chart-container">
+      {winnerIs ? ( <>{winnerIs} won the Game
+      <button onClick={handleReset}>Play Again</button></>
+  ):(
+      <>
+      <h4>Player {isXTurn ? "X":"O"} Please move</h4>
+      <div className="chart-row">
+        <Square onClick={() => handleClick(0)} value={state[0]} />
+        <Square onClick={() => handleClick(1)} value={state[1]} />
+        <Square onClick={() => handleClick(2)} value={state[2]} />
+      </div>
+      <div className="chart-row">
+        <Square onClick={() => handleClick(3)} value={state[3]} />
+        <Square onClick={() => handleClick(4)} value={state[4]} />
+        <Square onClick={() => handleClick(5)} value={state[5]} />
+      </div>
+      <div className="chart-row">
+        <Square onClick={() => handleClick(6)} value={state[6]} />
+        <Square onClick={() => handleClick(7)} value={state[7]} />
+        <Square onClick={() => handleClick(8)} value={state[8]} />
+      </div>
+      </>)}
+    </div>
+  );
+};
+export default Chart;
